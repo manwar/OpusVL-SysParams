2cc0112930242dddd5b90178f76070da9f1e3641
Brad Haywood <brad.haywood@opusvl.com>
2015-09-30 12:16:53 +0100

    Bumped version number

--- lib/OpusVL/SysParams.pm
+++ lib/OpusVL/SysParams.pm
@@ -26,11 +26,11 @@ OpusVL::SysParams - Module to handle system wide parameters
 
 =head1 VERSION
 
-Version 0.09
+Version 0.10
 
 =cut
 
-our $VERSION = '0.09';
+our $VERSION = '0.10';
 
 
 =head1 SYNOPSIS
