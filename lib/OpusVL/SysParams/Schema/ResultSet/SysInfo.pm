64250087021cb3b06e8048cc62dc0e41aed83e8a
Colin Newell <colin.newell@gmail.com>
2015-12-08 17:53:04 +0000

    Added new ordering method and fixed pod

--- lib/OpusVL/SysParams/Schema/ResultSet/SysInfo.pm
+++ lib/OpusVL/SysParams/Schema/ResultSet/SysInfo.pm
@@ -20,30 +20,6 @@ This is used by the L<OpusVL::SysParams> object.
 
 =head1 METHODS
 
-=head2 list
-
-List all parameters. If passed a true value as its first argument, the list will
-be a nested hashref of the form
-
-    {
-        path => 'path.to.node',
-        value => 'node value',
-        comment => '...',
-        children => {
-            childnode => {
-                path => 'path.to.node.childnode',
-                ...
-            }
-        }
-    }
-
-The topmost node will have no path, comment, or value, only children. Inner
-nodes will always have a path, but may have no value or no children (nodes with
-no children should always have a value).
-
-If the true parameter is not passed, this simply returns a default ResultSet for
-all parameters in the table.
-
 =head2 get
 
 Get a system parameter.  The key name is simply a string.  It's suggested you use some 
@@ -64,6 +40,10 @@ Delete a system parameter.
 
 Returns the keys of the system parameters.
 
+=head2 ordered
+
+Returns a resultset with an ordering applied.
+
 =head1 AUTHOR
 
 OpusVL, C<< <colin at opusvl.com> >>
@@ -76,36 +56,16 @@ This software is licensed according to the "IP Assignment Schedule" provided wit
 
 =cut
 
-sub list {
+sub ordered
+{
     my $self = shift;
-    my $group = shift;
-
-    my $rs = $self->search;
-    return $rs if not $group;
-
-    # TODO: should be able to write a WITH RECURSIVE query to get an RS here
-    # instead. That means the return value would always be the same.
-    my $groups = {};
-
-    for my $setting ($rs->all) {
-        my @path = split /\./, $setting->name;
-        my $node = $groups;
-
-        while (@path) {
-            my $name = shift @path;
-            my $path = $node->{path} || '';
-            $node->{children}->{$name} //= {};
-
-            $node = $node->{children}->{$name};
-            $node->{path} ||= join '.', grep {$_} $path, $name;
-        }
-
-        $node->{value}   = $setting->raw_value;
-        $node->{comment} = $setting->comment;
-    }
-
-    return $groups;
+    my $me = $self->current_source_alias;
+    return $self->search(undef, {
+        order_by => ["$me.label", "$me.name"],
+    });
 }
+
+
 
 sub set
 {
@@ -116,7 +76,7 @@ sub set
 	my $info = $self->update_or_create
 	({
 		name  => $name,
-		value => $value,
+		value => JSON->new->allow_nonref->encode($value)
 	});
 
 	return $value;
@@ -132,9 +92,7 @@ sub get
 		name => $name
 	});
 
-    return undef if not $info;
-
-    return $info->value;
+	return $info ? JSON->new->allow_nonref->decode($info->value) : undef;
 }
 
 sub del 
