4e4f1afe923a28cf51575a219ce97db3f59dd3f3
Colin Newell <colin@opusvl.com>
2016-12-21 09:00:30 +0000

    Bumped version number

--- lib/OpusVL/SysParams.pm
+++ lib/OpusVL/SysParams.pm
@@ -24,7 +24,7 @@ has 'schema' => (isa => 'DBIx::Class::Schema', is => 'ro', required => 1,
 # ABSTRACT: Module to handle system wide parameters
 
 
-our $VERSION = '0.16';
+our $VERSION = '0.17';
 
 
 =head1 SYNOPSIS
